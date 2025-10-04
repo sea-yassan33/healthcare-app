1a62593b815061681abc2fd34a935e4933180590
sea-yassan33 <a.yasuda_tr51@me.com>
2025-10-04 16:45:35 +0900

    update251004

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
-  title: "健康情報APP | Home",
+  title: "Health Hub | 健康アプリ",
   description: "健康情報を提供するWEBアプリケーションです。最新のテクノロジーと最新の健康に関する知見を組み合わせて、健康情報を提供します。",
 };
 
@@ -17,6 +17,8 @@ export default function RootLayout({
     <html lang="ja">
       <head>
         <link rel="icon" href="/icon.png" type="image/x-icon" sizes="16x16" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icon.png" />
+        <link rel="manifest" href="/manifest.json" />
       </head>
       <body className="min-h-screen bg-white">
         <Header/>

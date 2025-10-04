4a61a838d71e691ed8853dc2a4bffcd28bb49382
sea-yassan33 <a.yasuda_tr51@me.com>
2025-10-04 16:34:52 +0900

    update251004

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import Header from "./_components/Header";
 import Footer from "./_components/Footer";
 
 export const metadata: Metadata = {
-  title: "HealthCare App",
-  description: "健康情報系のアプリケーション",
+  title: "健康情報APP | Home",
+  description: "健康情報を提供するWEBアプリケーションです。最新のテクノロジーと最新の健康に関する知見を組み合わせて、健康情報を提供します。",
 };
 
 export default function RootLayout({
@@ -15,6 +15,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ja">
+      <head>
+        <link rel="icon" href="/icon.png" type="image/x-icon" sizes="16x16" />
+      </head>
       <body className="min-h-screen bg-white">
         <Header/>
           {children}
